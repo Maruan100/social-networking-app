3ea7d55c74265efb1e6504cac31da2ae5c1591c1
Maruan100 <maruane10@outlook.es>
2025-04-18 19:42:55 +0200

    ADD main function to capture commands and set project structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+#!/usr/bin/env node
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+export async function main(): Promise<void> {
+    const rl = readline.createInterface({ input, output });
+    const answer = await rl.question('> ');
+    rl.close();
+
+    if (answer.includes('->')) {
+        // posting()
+        console.log('posting')
+    } else if (answer.includes('follows')) {
+        // following()
+        console.log('following')
+    }
+    else if (answer.includes('wall')) {
+        // wall()
+        console.log('wall')
+    }
+    else if (!answer.includes('->')) {
+        // reading()
+        console.log('reading')
+    } else {
+        return await main();
+    }
+
+}
+
+console.clear();
+(async () => {
+    await main();
+})();
